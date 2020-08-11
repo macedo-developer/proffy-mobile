9f8fc131c24d274fc240dab58e776889fc80f6f7
macedo-developer <macedorenata.developer@gmail.com>
2020-08-11 20:16:35 -0300

    Style Tabs route

--- src/routes/StudyTabs.tsx
+++ src/routes/StudyTabs.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import { Ionicons } from "@expo/vector-icons";
+
 import TeacherList from "../pages/TeacherList";
 import Favorite from "../pages/Favorite";
 
@@ -8,9 +10,54 @@ const { Navigator, Screen } = createBottomTabNavigator();
 
 function StudyTabs() {
   return (
-    <Navigator>
-      <Screen name="TeacherList" component={TeacherList} />
-      <Screen name="Favorite" component={Favorite} />
+    <Navigator
+      tabBarOptions={{
+        style: {
+          elevation: 0,
+          shadowOpacity: 0,
+          height: 64,
+        },
+        tabStyle: {
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+        },
+        iconStyle: {
+          flex: 0,
+          height: 20,
+          width: 20,
+        },
+        labelStyle: {
+          fontFamily: "Archivo_700Bold",
+          fontSize: 13,
+          marginLeft: 16,
+        },
+        inactiveBackgroundColor: "#fafafc",
+        activeBackgroundColor: "#ebebf5",
+        inactiveTintColor: "#c1bccc",
+        activeTintColor: "#32264d",
+      }}
+    >
+      <Screen
+        options={{
+          tabBarLabel: "Proffys",
+          tabBarIcon: ({ color, size }) => {
+            return <Ionicons name="ios-easel" size={size} color={color} />;
+          },
+        }}
+        name="TeacherList"
+        component={TeacherList}
+      />
+      <Screen
+        options={{
+          tabBarLabel: "Favoritos",
+          tabBarIcon: ({ color, size }) => {
+            return <Ionicons name="ios-heart" size={size} color={color} />;
+          },
+        }}
+        name="Favorite"
+        component={Favorite}
+      />
     </Navigator>
   );
 }
