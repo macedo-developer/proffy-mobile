d2721710ce57b854757412aa8b09eee452dd5997
macedo-developer <macedorenata.developer@gmail.com>
2020-08-11 21:48:09 -0300

    Change color icons focused tabs

--- src/routes/StudyTabs.tsx
+++ src/routes/StudyTabs.tsx
@@ -41,8 +41,14 @@ function StudyTabs() {
       <Screen
         options={{
           tabBarLabel: "Proffys",
-          tabBarIcon: ({ color, size }) => {
-            return <Ionicons name="ios-easel" size={size} color={color} />;
+          tabBarIcon: ({ color, size, focused }) => {
+            return (
+              <Ionicons
+                name="ios-easel"
+                size={size}
+                color={focused ? "#8257e5" : color}
+              />
+            );
           },
         }}
         name="TeacherList"
@@ -51,8 +57,14 @@ function StudyTabs() {
       <Screen
         options={{
           tabBarLabel: "Favoritos",
-          tabBarIcon: ({ color, size }) => {
-            return <Ionicons name="ios-heart" size={size} color={color} />;
+          tabBarIcon: ({ color, size, focused }) => {
+            return (
+              <Ionicons
+                name="ios-heart"
+                size={size}
+                color={focused ? "#8257e5" : color}
+              />
+            );
           },
         }}
         name="Favorite"
