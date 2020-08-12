807c71540e97ebad9f2742ab4d246eb058c2efac
macedo-developer <macedorenata.developer@gmail.com>
2020-08-11 22:56:23 -0300

    Using component TeacherItem  in TeacherList

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -3,13 +3,24 @@ import React from "react";
 import { View } from "react-native";
 
 import PageHeader from "../../components/PageHeader";
+import TeacherItem from "../../components/TeacherItem";
 
 import styles from "./styles";
+import { ScrollView } from "react-native-gesture-handler";
 
 function TeacherList() {
   return (
     <View style={styles.container}>
       <PageHeader title="Proffys Disponíveis" />
+
+      <ScrollView
+        style={styles.teacherList}
+        contentContainerStyle={{ paddingHorizontal: 16, paddingBottom: 16 }}
+      >
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </ScrollView>
     </View>
   );
 }
