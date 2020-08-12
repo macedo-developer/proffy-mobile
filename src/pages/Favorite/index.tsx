df05b3a245b5427cee04923a7808cc0e40f4e8d9
macedo-developer <macedorenata.developer@gmail.com>
2020-08-11 23:00:56 -0300

    Using component TeacherItem  in Favorite

--- src/pages/Favorite/index.tsx
+++ src/pages/Favorite/index.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 
-import { View } from "react-native";
-import styles from "./styles";
+import { View, ScrollView } from "react-native";
 import PageHeader from "../../components/PageHeader";
+import TeacherItem from "../../components/TeacherItem";
+
+import styles from "./styles";
 
 function Favorite() {
   return (
     <View style={styles.container}>
       <PageHeader title="Meus proffys favoritos" />
+
+      <ScrollView
+        style={styles.teacherList}
+        contentContainerStyle={{ paddingHorizontal: 16, paddingBottom: 16 }}
+      >
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </ScrollView>
     </View>
   );
 }
