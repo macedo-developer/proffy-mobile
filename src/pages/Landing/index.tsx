51ea44414ef947804017116f2f566f5d28c5a65f
macedo-developer <macedorenata.developer@gmail.com>
2020-08-13 21:30:05 -0300

    Connecting api, total of connections

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Image, Text } from "react-native";
 
 import { useNavigation } from "@react-navigation/native";
 import { RectButton } from "react-native-gesture-handler";
+
+import api from "../../services/api";
 
 import landingImg from "../../assets/images/landing.png";
 import studyIcon from "../../assets/images/icons/study.png";
@@ -13,6 +15,16 @@ import style from "./styles";
 
 function Landing() {
   const { navigate } = useNavigation();
+
+  const [totalConnection, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get("connections").then((response) => {
+      const { total } = response.data;
+
+      setTotalConnections(total);
+    });
+  }, []);
 
   function handleNavigateToGiveClasses() {
     navigate("GiveClasses");
@@ -48,7 +60,8 @@ function Landing() {
       </View>
 
       <Text style={style.totalConnections}>
-        Total de 322 conexões realizadas. <Image source={heartIcon} />
+        Total de {totalConnection} conexões realizadas.{" "}
+        <Image source={heartIcon} />
       </Text>
     </View>
   );
